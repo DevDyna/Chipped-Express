913d92acf22a70e73d8418f3aa94cefb29cb1304
DevDyna <andrearisorti2003@gmail.com>
2024-02-21 22:15:25 +0100

    Update packager.bat

--- packager.bat
+++ packager.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
+echo compressing all recipes , keep on mind that require a few of seconds!
 IF EXIST "ChippedExpress.jar" DEL "ChippedExpress.jar"
 jar cf "ChippedExpress.jar" assets data META-INF pack.mcmeta pack.png
